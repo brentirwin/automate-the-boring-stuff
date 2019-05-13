f30065813fd36f382bd68e5581bdfc4433764511
Brent Irwin <brent@brentirwin.tv>
2019-05-13 14:31:18 -0500

    Add dragonLoot project

--- ch5/fantasy-game-inventory.py
+++ ch5/fantasy-game-inventory.py
@@ -10,4 +10,15 @@ def displayInventory(inventory):
         item_total += v
     print("Total number of items: " + str(item_total))
 
-displayInventory(stuff)
+#displayInventory(stuff)
+
+def addToInventory(inventory, addedItems):
+    for item in addedItems:
+        if item in inventory:
+            inventory[item] += 1
+    return inventory
+
+inv = {'gold coin': 42, 'rope': 1}
+dragonLoot = ['gold coin', 'dagger', 'gold coin', 'gold coin', 'ruby']
+inv = addToInventory(inv, dragonLoot)
+displayInventory(inv)
